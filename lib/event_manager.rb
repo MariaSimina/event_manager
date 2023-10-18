31f84f682549ecc181d109086ddd7e123d155329
Maria Simina <maria.simina_groza@yahoo.com>
2023-10-18 13:12:54 +0300

    Modify html letter template with names and legislators

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -30,6 +30,9 @@ contents = CSV.open(
   headers: true,
   header_converters: :symbol
   )
+
+template_letter = File.read('form_letter.html')
+
 contents.each do |row|
   name = row[:first_name]
   
@@ -37,5 +40,8 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  personal_letter = template_letter.gsub('FIRST_NAME', name)
+  personal_letter.gsub!('LEGISLATORS', legislators)
+
+  puts personal_letter
 end
